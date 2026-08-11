d3f02aed97e32be7a34ffff9744d4ec143d2287a
Riqza AF <maulana.10125908@mahasiswa.unikom.ac.id>
2026-08-11 17:32:56 +0700

    update thankyou & buton eventorder albert-jessica

--- src/components/templates/Albert-Jessica/Thankyou.tsx
+++ src/components/templates/Albert-Jessica/Thankyou.tsx
@@ -5,7 +5,7 @@ import { fadeUp, fadeLeft, fadeRight } from "../../../lib/animation";
 
 const Thankyou = () => {
   return (
-    <section className="relative w-full">
+    <section className="relative w-full min-h-screen">
       <Image
         src="/images/Albert-Jessica/Thankyou/BgThankyou.webp"
         alt="Profile Background"
@@ -13,55 +13,60 @@ const Thankyou = () => {
         className="object-cover object-bottom z-10"
       />
 
-      <div className="relative z-20 pt-[98px] flex flex-col items-center justify-center text-center">
-        <motion.h2
-          variants={fadeUp}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 1.5, ease: "easeOut" }}
-          className="upperacse font-marcellus text-[28px] text-[#4E4E4E] uppercase"
-        >
-          Thank You
-        </motion.h2>
-        <motion.p
-          variants={fadeUp}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 1.5, ease: "easeOut" }}
-          className="font-lora text-[12px] text-[#322E29] leading-normal pt-[26.5px]"
-        >
-          For being part of our journey. <br />
-          We look forward to celebrating love, laughter, and <br />
-          happily ever after with you!
-        </motion.p>
-        <motion.p
-          variants={fadeUp}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 1.5, ease: "easeOut" }}
-          className="font-lora text-[12px] text-[#322E29] leading-normal pt-[22px]"
-        >
-          #ALBethereforJES
-        </motion.p>
-        <motion.div
-          variants={fadeUp}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 1.5, ease: "easeOut" }}
-          className="flex flex-col items-center mt-[470px] mb-[34px]"
-        >
-          <Image
-            src="/images/Albert-Jessica/Thankyou/Provite.png"
-            alt="Provite"
-            width={250}
-            height={250}
-            className="w-[89px] h-auto"
-          />
-        </motion.div>
+      <div className="relative z-20 flex flex-col items-center justify-between text-center">
+        <div className="pt-[98px] flex flex-col items-center justify-center">
+          <motion.h2
+            variants={fadeUp}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+            className="upperacse font-marcellus text-[28px] text-[#4E4E4E] uppercase"
+          >
+            Thank You
+          </motion.h2>
+          <motion.p
+            variants={fadeUp}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+            className="font-lora text-[12px] text-[#322E29] leading-normal pt-[26.5px]"
+          >
+            For being part of our journey. <br />
+            We look forward to celebrating love, laughter, and <br />
+            happily ever after with you!
+          </motion.p>
+          <motion.p
+            variants={fadeUp}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+            className="font-lora text-[12px] text-[#322E29] leading-normal pt-[22px]"
+          >
+            #ALBethereforJES
+          </motion.p>
+        </div>
+
+        <div className="relative px-10 pb-[6px] text-center leading-none">
+          <motion.div
+            variants={fadeUp}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3 }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+            className="flex flex-col items-center mt-[470px] mb-[34px]"
+          >
+            <Image
+              src="/images/Albert-Jessica/Thankyou/Provite.png"
+              alt="Provite"
+              width={250}
+              height={250}
+              className="w-[89px] h-auto"
+            />
+          </motion.div>
+        </div>
       </div>
     </section>
   );
